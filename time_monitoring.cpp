162eab14ba10f8d507d712e4bd6a45529d75d48e
Maurizio <zeta_persei@hotmail.com>
2016-07-25 19:54:18 +0200

    Update time_monitoring.cpp

--- time_monitoring.cpp
+++ time_monitoring.cpp
@@ -10,13 +10,23 @@
 DS3231  rtc(SDA, SCL);
 
 Time t;
-String telefonNumber = "00000000000";
-
 int orario_on;
-const int HOUR_INITIAL_TIME = 19;
-const int HOUR_END_TIME = 20;
-const int MIN_INITIAL_TIME = 01;
-const int MIN_END_TIME = 45;
+
+String telefonNumber = "00000000000";
+String atCmdSetText = "AT+CMGF=1";
+String atCmdSendSms = "AT+CMGS=";
+
+const char CR = 13;
+const char CTRLZ = 26;
+const char ESCP = 27;
+const char SPACE = 32;
+const char VIRGO = 34;
+
+const int HOUR_INITIAL_TIME = 12;
+const int HOUR_END_TIME = 13;
+const int MIN_INITIAL_TIME = 10;
+const int MIN_END_TIME = 59;
+const int endAlarm = 1000;
 
 // declare alarm pin
 
@@ -62,6 +72,9 @@ void setup()
   //rtc.setTime(12, 0, 0);     // Set the time to 12:00:00 (24hr format)
   //rtc.setDate(1, 1, 2014);   // Set the date to January 1st, 2014
 
+  Serial.print(atCmdSetText + CR);
+  
+
 }
 
 void loop()
@@ -80,37 +93,49 @@ void loop()
   t = rtc.getTime();
   
   // Send Day-of-Week
-  Serial.print(rtc.getDOWStr());
-  Serial.print(" ");
+  //Serial.print(rtc.getDOWStr());
+  //Serial.print(" ");
   
   // Send date
-  Serial.print(rtc.getDateStr());
-  Serial.print(" -- ");
+  //Serial.print(rtc.getDateStr());
+  //Serial.print(" -- ");
 
   // Send time
-  Serial.println(rtc.getTimeStr(FORMAT_SHORT));
+  //Serial.println(rtc.getTimeStr(FORMAT_SHORT));
   
   // Serial.print(t.hour, DEC);
   
   orario_on = t.hour;
   
-  if ( orario_on >= HOUR_INITIAL_TIME && < HOUR_END_TIME ) 
-     {
-        Serial.print("\n We are inside in hour Test Time window");
+  switch (alarm1Value) {
+      case LOW:
+        if ( orario_on >= HOUR_INITIAL_TIME && orario_on < HOUR_END_TIME && t.min > MIN_INITIAL_TIME && t.min < MIN_END_TIME) 
+           {
+      
+            Serial.print(atCmdSendSms + VIRGO + telefonNumber + VIRGO + CR );
+            Serial.print("Device 1 not active");
+            Serial.print(CTRLZ);
+            Serial.print(ESCP);
+              while  (alarm1.update()){
+                ;    }     
+            }
         
-        if ( t.min > MIN_INITIAL_TIME && t.min < MIN_END_TIME)
-        {
-          Serial.print("\n We are inside in minuts Test Time Window");
+        break;
+      case HIGH:
+          if ( orario_on <= HOUR_INITIAL_TIME && orario_on > HOUR_END_TIME && t.min < MIN_INITIAL_TIME && t.min > MIN_END_TIME) 
+           {
+      
+            Serial.print(atCmdSendSms + VIRGO + telefonNumber + VIRGO + CR );
+            Serial.print("Device 1 active out time window");
+            Serial.print(CTRLZ);
+            Serial.print(ESCP);
+              while  (!alarm1.update()){
+                ;    }
+                   
+            }
+            
+           break;                  
         }
-             
-     }
-  
  
-  delay (1000);
-  Serial.print("\n\n");
-  Serial.print("Chip Temperature: ");
-  Serial.print(rtc.getTemp());
-  Serial.println(" C\n");
-  delay (10000);
   
 }
